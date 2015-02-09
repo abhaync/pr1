32c0c2260ba5895ba3b49871d18b1dd00a2cf20c
Abhay N <abhaync@gmail.com>
2015-02-09 21:03:25 +0530

    Python

---
+++ file.py
@@ -0,0 +1,13 @@
+
+print("Hello World")
+my_name = input("What is your name?\n")
+print("Hello, %s" %my_name)
+print(2+3)
+var = 0
+var = var + 1
+var = var * 7 / 17
+var = var ** 2
+if var<1:
+	print("Sorry")
+print(my_name)
+print(var)
