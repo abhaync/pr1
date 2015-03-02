a130cc6e1a6608341d21ac5b32466218f6f72224
sndpkiran <sndpkiran@gmail.com>
2015-03-03 00:21:07 +0530

    Reads an array and prints the same array

--- qs.c
+++ qs.c
@@ -13,5 +13,14 @@ double clock()
 
 int main()
 {
+    int arr[100],n,i;
+    printf("Enter the size of the array\n");
+    scanf("%d",&n);
+    for(i=0;i<n;i++) {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++) {
+        printf("%d",arr[i]);
+    }
     return 0;
 }
